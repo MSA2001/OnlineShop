9163398bb352457a8a858f719e0d0ce646f80161
MSA2001 <msa2001b21@gmail.com>
2023-01-04 17:36:59 +0330

    Product detail page added

--- home/views.py
+++ home/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product, Category
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home/home.html')
+        products = Product.objects.filter(available=True)
+
+        return render(request, 'home/home.html', {'products': products})
+
+
+class ProductDetailView(View):
+
+    def get(self, request, slug):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'home/detail.html', {'product': product})
 
