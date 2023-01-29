a5ea480b40797361c00d8d3413d47008afa31fd8
MSA2001 <msa2001b21@gmail.com>
2023-01-29 13:27:15 +0330

    Add to cart 2

--- orders/cart.py
+++ orders/cart.py
@@ -11,3 +11,13 @@ class Cart:
             cart = self.session[CART_SESSION_ID] = {}
         self.cart = cart
 
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):     # save method for sessions ===> self.save()
+        self.session.modified = True
+
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from .cart import Cart
+from home.models import Product
+from .forms import CartAddForm
 # Create your views here.
 
 
@@ -15,5 +18,9 @@ class CartAddView(View):
         pass
 
     def post(self, request, product_id):
-
-        pass
+        cart = Cart()
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddForm(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
