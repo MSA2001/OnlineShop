1a3ee2617c67a479b2646933cd16e861b3a171fa
MSA2001 <msa2001b21@gmail.com>
2023-01-30 18:56:39 +0330

    Cart total price added

--- orders/cart.py
+++ orders/cart.py
@@ -32,3 +32,5 @@ class Cart:
     def save(self):     # save method for sessions ===> self.save()
         self.session.modified = True
 
+    def get_total_price(self):
+        return sum(int(item['price']) * item['quantity'] for item in self.cart.values())
