76f9430a3440f9e0bcb39d0b9e56f9224b889c6e
MSA2001 <msa2001b21@gmail.com>
2023-01-30 18:21:48 +0330

    Add to cart 3

--- orders/views.py
+++ orders/views.py
@@ -9,7 +9,8 @@ from .forms import CartAddForm
 class CartView(View):
 
     def get(self, request):
-        return render(request, 'orders/cart.html')
+        cart = Cart(request)
+        return render(request, 'orders/cart.html', {'cart': cart})
 
 
 class CartAddView(View):
@@ -18,7 +19,7 @@ class CartAddView(View):
         pass
 
     def post(self, request, product_id):
-        cart = Cart()
+        cart = Cart(request)
         product = get_object_or_404(Product, id=product_id)
         form = CartAddForm(request.POST)
         if form.is_valid():
