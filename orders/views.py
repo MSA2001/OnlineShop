528d0b29b724e376d9b37e66c282f9d16317220c
MSA2001 <msa2001b21@gmail.com>
2023-02-05 15:33:07 +0330

    Zarin pal 1 (Pay)

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,9 @@ from home.models import Product
 from .forms import CartAddForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order, OrderItem
+from django.http import HttpResponse
+import requests
+import json
 
 
 # Create your views here.
@@ -51,11 +54,38 @@ class OrderCreateView(LoginRequiredMixin, View):
         cart = Cart(request)
         order = Order.objects.create(user=request.user)
         for item in cart:
-            OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
+                                     quantity=item['quantity'])
         return redirect('orders:order_detail', order.id)
 
 
+# zarin pal https://github.com/rasooll/zarinpal-django-py3/blob/master/views.py
+MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
+ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+CallbackURL = 'http://localhost:8000/orders/verify/'
 
 
-
-
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        req_data = {
+            "merchant_id": MERCHANT,
+            "amount": order.get_total_price(),
+            "callback_url": CallbackURL,
+            "description": description,
+            "metadata": {"mobile": request.user.phone_number}
+        }
+        req_header = {"accept": "application/json",
+                      "content-type": "application/json'"}
+        req = requests.post(url=ZP_API_REQUEST, data=json.dumps(
+            req_data), headers=req_header)
+        authority = req.json()['data']['authority']
+        if len(req.json()['errors']) == 0:
+            return redirect(ZP_API_STARTPAY.format(authority=authority))
+        else:
+            e_code = req.json()['errors']['code']
+            e_message = req.json()['errors']['message']
+            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
